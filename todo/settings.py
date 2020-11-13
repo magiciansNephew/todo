e487dc6e86d996de9d3e2d6c04e2fa6a64bfe88c
Pramod Limbu <limbupramod1@gmail.com>
2020-11-13 19:27:53 +0545

    Production Basics Settings

--- todo/settings.py
+++ todo/settings.py
@@ -22,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5t+=1bra2_c0slol7+8nkbaldwzy5-26c3v4wkj0ab)#$=u=$!'
+#SECRET_KEY = '5t+=1bra2_c0slol7+8nkbaldwzy5-26c3v4wkj0ab)#$=u=$!'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '5t+=1bra2_c0slol7+8nkbaldwzy5-26c3v4wkj0ab)#$=u=$!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
